048e3120115759fe42dc3faf769aedb8bde600c9
Sarah Yoder <syoder@thirdandgrove.com>
2020-01-10 07:35:59 -0500

    Add more content to body.

--- src/Body.js
+++ src/Body.js
@@ -4,9 +4,15 @@ const Header = () => {
 
   return (
     <div className="bodyContainer">
-      <h3>Things I've Done Lately</h3>
-      <div>This website, a React app.</div>
-      <div>A portfolio for my husband, created with Gatsby and a Drupal 8 backend.</div>
+      <h2>Things I've Done Lately</h2>
+      <h3>This website, a React app.</h3>
+      <p><a href="https://github.com/sarahcyoder/sarahlaftchiev" target="_blank">See it on GitHub</a></p>
+      <h3>A portfolio for my husband, created with Gatsby and a Drupal 8 backend.</h3>
+      <p><a href="https://github.com/sarahcyoder/emil-laftchiev" target="_blank">See it on GitHub</a></p>
+      <h3>Dozens of projects at Third and Grove, such as...</h3>
+      <div><a href="https://www.thirdandgrove.com/" target="_blank">thirdandgrove.com</a></div>
+      <p><a href="https://github.com/thirdandgrove/thirdandgrove-com-gatsby" target="_blank">See it on Github</a></p>
+      <div>Sites like <a href="https://www.draper.com/" target="_blank">draper.com</a> and <a href="https://www.wvi.org/" target="_blank">wvi.org</a></div>
     </div>
   )
 }
