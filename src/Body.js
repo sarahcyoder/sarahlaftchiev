4a56fef81a4d5cfd6f726671134f26e8be17a856
Sarah Yoder <syoder@thirdandgrove.com>
2020-01-22 12:35:08 -0500

    Fix capitalization in references to GitHub; clean up imports.

--- src/Body.js
+++ src/Body.js
@@ -16,13 +16,13 @@ const Header = () => {
       </div>
       <div className="project">
         <h3>Dozens of projects at Third and Grove, such as...</h3>
-        <p><a href="https://www.thirdandgrove.com/" target="_blank" rel="noopener noreferrer">thirdandgrove.com</a> | <a href="https://github.com/thirdandgrove/thirdandgrove-com-gatsby" target="_blank" rel="noopener noreferrer">See it on Github</a></p>
+        <p><a href="https://www.thirdandgrove.com/" target="_blank" rel="noopener noreferrer">thirdandgrove.com</a> | <a href="https://github.com/thirdandgrove/thirdandgrove-com-gatsby" target="_blank" rel="noopener noreferrer">See it on GitHub</a></p>
         <p>Sites like <a href="https://www.draper.com/" target="_blank" rel="noopener noreferrer">draper.com</a> and <a href="https://www.wvi.org/" target="_blank" rel="noopener noreferrer">wvi.org</a></p>
       </div>
       <div className="learn-more">
         <h2>Learn More</h2>
         <p><a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a></p>
-        <p><a href="https://github.com/sarahcyoder" target="_blank" rel="noopener noreferrer">Github</a></p>
+        <p><a href="https://github.com/sarahcyoder" target="_blank" rel="noopener noreferrer">GitHub</a></p>
         <p><a href="https://www.linkedin.com/in/sarahcyoder/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
       </div>
     </div>
