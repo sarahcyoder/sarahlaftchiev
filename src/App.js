70157cf3b56c1b06b0dee31025c10a64e0882766
Sarah Yoder <syoder@thirdandgrove.com>
2020-01-22 12:17:29 -0500

    Add footer; remove react logo.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
 import Body from './Body.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
        <Header />
       </header>
       <Body />
-      <img src={logo} className="App-logo" alt="logo" />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+
+  return (
+    <div className="footerContainer">
+      <h2>Thanks for scrolling.</h2>
+      <h3>sarahlaftchiev@gmail.com</h3>
+    </div>
+  )
+}
+
+export default Footer;
